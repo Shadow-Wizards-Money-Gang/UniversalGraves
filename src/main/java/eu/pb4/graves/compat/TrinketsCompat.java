3219cd20297763b52342bb5378f93b24cc2b0a30
Santy <42287552+majorsanty@users.noreply.github.com>
2024-10-14 00:33:23 -0300

    test

--- src/main/java/eu/pb4/graves/compat/TrinketsCompat.java
+++ src/main/java/eu/pb4/graves/compat/TrinketsCompat.java
@@ -6,6 +6,7 @@ import dev.emi.trinkets.api.TrinketsApi;
 import dev.emi.trinkets.api.event.TrinketDropCallback;
 import eu.pb4.graves.GravesApi;
 import eu.pb4.graves.grave.GraveInventoryMask;
+import eu.pb4.graves.mixin.PlayerInventoryAccessor;
 import eu.pb4.graves.other.VanillaInventoryMask;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.item.ItemStack;
@@ -64,37 +65,42 @@ public record TrinketsCompat(boolean preventAddition) implements GraveInventoryM
 
     @Override
     public boolean moveToPlayerExactly(ServerPlayerEntity player, ItemStack stack, int slot, NbtElement extraData) {
-        var groupId = ((NbtCompound) extraData).getString(GROUP_TAG);
-        var slotId = ((NbtCompound) extraData).getString(SLOT_TAG);
-
-        var inventory = getInventory(player, groupId, slotId);
-
-        if (inventory != null) {
-            if (inventory.getStack(slot).isEmpty()) {
-                inventory.setStack(slot, stack.copyAndEmpty());
-                return true;
-            }
+        var inventory = player.getInventory();
+        if (slot > -1 && slot < inventory.size() && inventory.getStack(slot).isEmpty()) {
+            inventory.setStack(slot, stack.copy());
+            stack.setCount(0);
+            return true;
         }
+
         return false;
     }
 
     @Override
-    public boolean moveToPlayerClosest(ServerPlayerEntity player, ItemStack stack, int slot, NbtElement data) {
-        var groupId = ((NbtCompound) data).getString(GROUP_TAG);
-        var slotId = ((NbtCompound) data).getString(SLOT_TAG);
+    public boolean moveToPlayerClosest(ServerPlayerEntity player, ItemStack stack, int intended, NbtElement extraData) {
+        var inventory = player.getInventory();
+        if (!stack.isEmpty()) {
+            int slot;
+            try {
+                if (stack.isDamaged()) {
+                    slot = inventory.getEmptySlot();
 
-        var inventory = getInventory(player, groupId, slotId);
-
-        if (inventory != null) {
-            int size = inventory.size();
-
-            for (int i = 0; i < size; i++) {
-                if (inventory.getStack(i).isEmpty()) {
-                    inventory.setStack(i, stack.copyAndEmpty());
-                    return true;
+                    if (slot >= 0) {
+                        inventory.main.set(slot, stack.copy());
+                        stack.setCount(0);
+                        return true;
+                    }
+                } else {
+                    int i;
+                    do {
+                        i = stack.getCount();
+                        stack.setCount(((PlayerInventoryAccessor) inventory).callAddStack(stack));
+                    } while (!stack.isEmpty() && stack.getCount() < i);
                 }
+            } catch (Exception e) {
+                // Silence!
             }
         }
+
         return false;
     }
 
